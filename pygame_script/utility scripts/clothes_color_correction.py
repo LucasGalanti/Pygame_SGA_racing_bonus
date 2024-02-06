72b172020545b0e09ff97386ae0f726c713531e4
Lucas Galanti <lsngalanti@gmail.com>
2024-02-05 22:34:00 -0300

    Revert "created .exe zip file"
    This reverts commit c5ae349f20f8b2291aabdc3b2609756433e98b34.

--- pygame_script/utility scripts/clothes_color_correction.py
+++ pygame_script/utility scripts/clothes_color_correction.py
@@ -1,3 +1,41 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a7242cd827ba87a8fbabb668f27869e94a36626cccb1e0391a04ce1f3c639bea
-size 1565
+from PIL import Image
+import os
+
+
+RGB_color_change_dict = {
+    "light_shirt": [(234, 50, 60), (0,122,197)],
+    "dark_shirt": [(196, 36, 48), (0,88,143)],
+    "light_pants": [(0, 152, 220), (95, 92, 91)],
+    "dark_pants": [(0, 105, 170), (56, 51 , 49)],
+    "darker_shirt": [(137,30,43), (0, 35, 57)]
+}
+
+
+# Define the folder path where the images are located
+folder_path = 'C:/Users/L4069846/OneDrive - Saint-Gobain/Documents - Inteligência de Vendas/Outros Projetos/Card_Game_Convencao_2023/Jogo/assets/char_selection/'
+
+# Iterate through all the PNG images in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith(".png"):
+        # Open the image
+        image_path = os.path.join(folder_path, filename)
+        image = Image.open(image_path).convert("RGBA")
+
+        # Get the pixel data
+        pixels = image.load()
+
+        # Iterate through each pixel in the image
+        for x in range(image.width):
+            for y in range(image.height):
+                # Check if the pixel color matches any of the RGB values in the dictionary
+                for key, values in RGB_color_change_dict.items():
+                    # Only change the color of non-transparent pixels
+                    if pixels[x, y][:3] == values[0] and pixels[x, y][3] > 0:
+                        # Change the pixel color to the second RGB value in the dictionary
+                        # Preserve the alpha channel
+                        pixels[x, y] = values[1] + (pixels[x, y][3],)
+
+        # Save the modified image
+        image.save(image_path)
+
+
--- pygame_script/utility scripts/file_name_correction.py
+++ pygame_script/utility scripts/file_name_correction.py
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5ed19121dddc61ffa312e97e2e3bc82c26a6fa42fd07e40682a130ebf72ade1e
-size 710
+import os
+
+folder_path = 'C:/Users/L4069846/OneDrive - Saint-Gobain/Documents - Inteligência de Vendas/Outros Projetos/Card_Game_Convencao_2023/Jogo/assets/char_selection/'
+
+for filename in os.listdir(folder_path):
+    #if filename == 'Character1M_1_silver_medal_get_2.png':
+    if filename.endswith('.png'):
+        base_name, extension = os.path.splitext(filename)
+        parts = base_name.split('_')
+        if len(parts) > 1 and parts[-1].isdigit():
+            parts[-1] = parts[-1].zfill(2)
+            new_name = '_'.join(parts) + extension
+            old_path = os.path.join(folder_path, filename)
+            new_path = os.path.join(folder_path, new_name)
+            os.rename(old_path, new_path)
